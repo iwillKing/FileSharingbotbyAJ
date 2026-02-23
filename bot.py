1289211bac71131bfbee4cf803b43c1c35939731
iwillKing <Aniketkmrjha@gmail.com>
2026-02-23 09:43:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,20 @@
+import os
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class HealthCheckHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running")
+
+def run_health_server():
+    port = int(os.environ.get("PORT", 8000))
+    server = HTTPServer(("0.0.0.0", port), HealthCheckHandler)
+    server.serve_forever()
+
+threading.Thread(target=run_health_server).start()
+
 import random
 import json
 import os
